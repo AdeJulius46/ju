48ea89953afe33f53b4dda6bfb4221133bfb4c95
Julius <adegokejulius46@gmail.com>
2022-12-24 13:58:09 +0100

    hahah

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,6 +10,7 @@ export default function Header() {
             />
             <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--project">Project 3</h4>
+    
         </header>
     )
 }
